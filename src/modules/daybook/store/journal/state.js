d19b4227eed93f6a7315b97d5c84fe28ef5d91e1
ANDREA OYARCE <AMOYARCEE@GMAIL.COM>
2023-02-11 10:23:09 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: '1', //12371298317892
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Laboris sit esse sint amet adipisicing officia sint aliqua. Minim ullamco veniam amet ad et esse eu quis. Veniam reprehenderit cupidatat ex aliqua quis minim commodo reprehenderit. Eiusmod dolore incididunt non Lorem duis reprehenderit cillum commodo culpa ex. Qui laboris sunt amet proident commodo. Anim velit sint et Lorem. Sint laboris qui nisi exercitation.',
+            picture: null, // https: //
+        },
+        {
+            id: '2', //12371298317892
+            date: new Date().toDateString(),
+            text: 'Excepteur nostrud mollit Lorem sunt reprehenderit ipsum. Laborum sint sunt aliqua sunt pariatur tempor. Magna aliqua minim sunt ea velit incididunt ad in culpa excepteur duis anim. Quis magna proident commodo non deserunt eiusmod excepteur. Magna aute magna pariatur mollit nulla ipsum ipsum. Sit exercitation Lorem consequat Lorem occaecat labore consectetur sunt duis consectetur minim veniam. Amet consectetur irure incididunt reprehenderit ex est.',
+            picture: null,  
+        },
+        {
+            id: '3',
+            date: new Date().toDateString(),
+            text: 'Aliqua veniam qui non commodo fugiat elit elit non ipsum sunt elit est ea deserunt. Voluptate reprehenderit sint amet aliqua dolore nostrud duis quis dolore. Occaecat do fugiat duis labore eiusmod officia reprehenderit velit.',
+            picture: null,
+        },
+    ]
+})
